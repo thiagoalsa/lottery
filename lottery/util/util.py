617dbc866f48f795dc89faf924d1e7547f48303c
Thiago de Almeida Santos <103751658+thiagoalsa@users.noreply.github.com>
2022-05-16 22:43:49 +0100

    type hinting in util

--- lottery/util/util.py
+++ lottery/util/util.py
@@ -1,18 +1,18 @@
 import random
 
 
-def raffle(number_of_numbers, game_size):
+def raffle(number_of_numbers: int, game_size: int):
     numbers_drawn = random.sample(range(1, game_size + 1), number_of_numbers)
     return sorted(numbers_drawn)
 
 
-def match_number(a, b):
+def match_number(numbers_drawn: list, numbers_chosen: list):
     lt = []
-    for c in a:
-        if c in b:
-            lt.append(c)
+    for number in numbers_drawn:
+        if number in numbers_chosen:
+            lt.append(number)
     return lt
 
 
-def total_match_number(lista):
-    return len(lista)
+def total_match_number(match_num: list):
+    return len(match_num)
