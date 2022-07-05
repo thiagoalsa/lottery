68391bf0f409d1660ae3f0946418c962d7e4d927
Thiago de Almeida Santos <103751658+thiagoalsa@users.noreply.github.com>
2022-07-05 10:28:49 +0100

    Create DocStrings in def raffle and def match_number

--- lottery/util/util.py
+++ lottery/util/util.py
@@ -2,16 +2,35 @@ import random
 
 
 def raffle(number_of_numbers: int, game_size: int):
+    """
+    Draws a desired amount of numbers in descending order.
+
+    Args:
+        number_of_numbers: number of numbers that will be randomly drawn.
+        game_size: the highest number that can be drawn
+
+    Returns:
+         different numbers in descending order
+    """
     numbers_drawn = random.sample(range(1, game_size + 1), number_of_numbers)
     return sorted(numbers_drawn)
 
 
 def match_number(numbers_drawn: list, numbers_chosen: list):
-    lt = []
+    """
+    Create a list with the numbers that two other lists have in common.
+
+    Args:
+        numbers_drawn: a list of drawn numbers
+        numbers_chosen: a list of chosen numbers
+    Returns:
+        a list of numbers that numbers_drawn and numbers_chosen have in common.
+    """
+    list_match = []
     for number in numbers_drawn:
         if number in numbers_chosen:
-            lt.append(number)
-    return lt
+            list_match.append(number)
+    return list_match
 
 
 def total_match_number(match_num: list):
